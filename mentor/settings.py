b4bc7b46821bdb015013c4abb5c0d3ab1711df05
Nguyen-Quy <96980131+Nguyen-Quy@users.noreply.github.com>
2022-09-29 17:36:28 +0700

    Update settings.py

--- mentor/settings.py
+++ mentor/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&hu05y+tzuxo(4w4&sr69n6p-lz%y4@*t25iw((l#9s26(%70l'
+SECRET_KEY = 'django-insecure-xxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
